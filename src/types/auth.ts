3a0f8ae61f7733af95669fb8f18f03ffd629007f
Ahmed Nageep <naguib@shougl.com>
2026-09-16 14:28:27 +0300

    Initial commit from Create Next App

---
+++ src/types/auth.ts
@@ -0,0 +1,21 @@
+export interface AuthUser {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: AuthUser;
+}
+
+export interface RegisterPayload {
+  email: string;
+  password: string;
+  name: string;
+}
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
